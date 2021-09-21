f40f673c49ba5d0cc54322a9850f43269854b193
PolyAlpha Finance <88970150+PolyAlphaFinance@users.noreply.github.com>
2021-09-21 13:28:38 +0700

    Update ALPHAToken.sol

--- ALPHAToken.sol
+++ ALPHAToken.sol
@@ -596,7 +596,7 @@ contract BEP20 is Context, IBEP20, Ownable {
     uint8 private _decimals;
 
     uint256 private MAXCAP;
-    uint256 constant MAXCAPSUPPLY = 16000 * (10 ** 18);
+    uint256 constant MAXCAPSUPPLY = 9300 * (10 ** 18);
 
     /**
      * @dev Sets the values for {name} and {symbol}, initializes {decimals} with
